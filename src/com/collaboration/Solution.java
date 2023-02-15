33b93a106bf39c11525786f350b9d969ea2a83f8
Miaraii <maryamogunlesi@gmail.com>
2023-02-14 23:38:42 -0500

    Added loops to flip and find

--- src/com/collaboration/Solution.java
+++ src/com/collaboration/Solution.java
@@ -6,9 +6,29 @@ public class Solution {
     }
     
     private static int find(int[] A, int target) {
-      
+    	
+        int maxTarget = 0;
+
+    	for (int i = 1; i <= target; i++){
+			if (A[i] > A[maxTarget]){
+				maxTarget = i;    
+			}
+			
+        }
+        return maxTarget;
     }
+    
     private static void flip(int[] A, int index) {
+        
+    	int l = 0, j = index;
+        while(l < j){
+            int temp = A[l];
+            A[l] = A[j];
+            A[j] = temp;
+            l++;
+            j--;
+        }
+        
         
     }
 
