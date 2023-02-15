5b6d1085b129fb479ce4eb167dd4ea331f05a8d6
enocholisa <enocholisa@gmail.com>
2023-02-14 21:58:23 -0500

    Boilerplate code from Coding Rooms

---
+++ src/com/collaboration/Solution.java
@@ -0,0 +1,19 @@
+package com.collaboration;
+
+public class Solution {
+    public static void pancakeSort(int[] inputArray) {
+        
+    }
+    
+    private static int find(int[] A, int target) {
+      
+    }
+    private static void flip(int[] A, int index) {
+        
+    }
+
+    public static void main(String[] args){
+        int[] arr = {3,2,4,1};
+        pancakeSort(arr);
+    }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author EdSofta-Enoch
+ *
+ */
+module CollaborationOfMaryamEnoch {
+}
